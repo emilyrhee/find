26dbb65ddd18f9d031999eb15749c7f6df4b7f17
emilyrhee <emilyrhee99@gmail.com>
2024-04-03 18:33:27 -0400

    only print name if arg name is same

--- find.c
+++ find.c
@@ -3,7 +3,7 @@
 #include <sys/stat.h>
 #include <string.h>
 
-void print_entry(char *dir_entry_name) {
+void print_entry(char* dir_entry_name) {
     struct stat info;
 
     if(stat(dir_entry_name, &info) == -1) {
@@ -12,9 +12,8 @@ void print_entry(char *dir_entry_name) {
         if(S_ISDIR(info.st_mode)) {
             printf("%s is a directory\n", dir_entry_name);
         }
-        if(1) {
-            printf("Directory entry: %s\n", dir_entry_name);
-        }
+
+        printf("Directory entry: %s\n", dir_entry_name);
     }
 }
 
@@ -22,7 +21,11 @@ int is_valid_entry(struct dirent *entry) {
     return (entry != NULL);
 }
 
-int main() {
+int is_same_name(const char *entry_name, const char *target_name) {
+    return strcmp(entry_name, target_name) == 0;
+}
+
+int main(int argc, char* argv[]) {
     char* dir_name = ".";
     DIR *dir_ptr;
     struct dirent* dirent_ptr;
@@ -31,9 +34,16 @@ int main() {
     if(dir_ptr == 0) {
         perror(dir_name);
     } else {
-        while(is_valid_entry(dirent_ptr = readdir(dir_ptr))) {
-            print_entry(dirent_ptr->d_name);
+        dirent_ptr = readdir(dir_ptr);
+
+        while(is_valid_entry(dirent_ptr)) {
+            if (is_same_name(dirent_ptr->d_name, argv[1])) {
+                print_entry(dirent_ptr->d_name);
+            }
+
+            dirent_ptr = readdir(dir_ptr);
         }
+        
         closedir(dir_ptr);
     }
     
