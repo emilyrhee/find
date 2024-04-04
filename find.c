976ff42b81dac42af124bd1e9a5c5319726d1dd4
emilyrhee <emilyrhee99@gmail.com>
2024-04-03 20:45:20 -0400

    I b refactoring

--- find.c
+++ find.c
@@ -1,44 +1,43 @@
 #include <stdio.h>
 #include <dirent.h>
-#include <sys/stat.h>
 #include <string.h>
-
-void print_entry(char* dir_entry_name) {
-    struct stat info;
-
-    if(stat(dir_entry_name, &info) == -1) {
-        perror(dir_entry_name);
-    } else {
-        if(S_ISDIR(info.st_mode)) {
-            printf("%s is a directory\n", dir_entry_name);
-        }
-
-        printf("Directory entry: %s\n", dir_entry_name);
-    }
-}
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/limits.h>
 
 int is_valid_entry(struct dirent *entry) {
     return (entry != NULL);
 }
 
-int is_same_name(const char *entry_name, const char *target_name) {
-    return strcmp(entry_name, target_name) == 0;
+int is_same_str(const char *entry_name, const char *target_name) {
+    return !strcmp(entry_name, target_name);
+}
+
+void print_file_info(const char *file_name) {
+    char file_path[PATH_MAX];
+    
+    if (getcwd(file_path, sizeof(file_path)) != NULL) {
+        printf("%s\n", file_path);
+        printf("   %s\n", file_name);
+    } else {
+        perror("getcwd() error");
+    }
 }
 
 int main(int argc, char* argv[]) {
     char* dir_name = ".";
-    DIR *dir_ptr;
+    DIR* dir_ptr;
     struct dirent* dirent_ptr;
     dir_ptr = opendir(dir_name);
 
-    if(dir_ptr == 0) {
+    if(dir_ptr == NULL) {
         perror(dir_name);
     } else {
         dirent_ptr = readdir(dir_ptr);
 
         while(is_valid_entry(dirent_ptr)) {
-            if (is_same_name(dirent_ptr->d_name, argv[1])) {
-                print_entry(dirent_ptr->d_name);
+            if (is_same_str(dirent_ptr->d_name, argv[1])) {
+                print_file_info(dirent_ptr->d_name);
             }
 
             dirent_ptr = readdir(dir_ptr);
